e60aaec667b496ef31532ae4f5902c5eb084ef7e
Bug Mika <bugmika@Bugs-MBP.attlocal.net>
2025-07-23 10:07:19 -0400

    fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,30 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate", -- ensures parsers are up to date
-    event = { "BufReadPost", "BufNewFile" }, -- lazy load
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua",
-          "typescript",
-          "tsx",
-          "javascript",
-          "ruby",
-          "html",
-          "css",
-          "json",
-          "yaml",
-          "markdown",
-          "bash",
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-        auto_install = true,
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",                     -- ensures parsers are up to date
+		event = { "BufReadPost", "BufNewFile" }, -- lazy load
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"typescript",
+					"tsx",
+					"javascript",
+					"ruby",
+					"html",
+					"css",
+					"json",
+					"yaml",
+					"markdown",
+					"bash",
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true, disable = { "ruby" } },
+				auto_install = true,
+			})
+		end,
+	},
 }
-
